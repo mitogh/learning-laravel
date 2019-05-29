ebe003392a8835d0c64a10bf43bd8af651b8d8eb
mitogh <hello@crisoforo.com>
2019-05-28 22:34:35 -0500

    Update User factory same as Model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,12 +17,12 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->define( User::class, function ( Faker $faker ) {
     return [
-        'name' => $faker->name,
+        'name' => implode( ' ', [ $faker->name, $faker->lastName ] ),
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'key' => $faker->sha256,
+        'avatar' => $faker->imageUrl(),
+        'remember_token' => Str::random( 10 ),
     ];
-});
+} );
