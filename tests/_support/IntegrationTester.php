58a6423b37583c3c9be2902cb1ba3445fb466d2c
mitogh <hello@crisoforo.com>
2019-05-29 21:47:47 -0500

    Add helper to create a mock from socialite

--- tests/_support/IntegrationTester.php
+++ tests/_support/IntegrationTester.php
@@ -1,5 +1,6 @@
 <?php
 
+use Laravel\Socialite\Two\User;
 
 /**
  * Inherited Methods
@@ -15,12 +16,49 @@
  * @method void pause()
  *
  * @SuppressWarnings(PHPMD)
-*/
+ */
 class IntegrationTester extends \Codeception\Actor
 {
     use _generated\IntegrationTesterActions;
 
-   /**
-    * Define custom actions here
-    */
+    /**
+     * Define custom actions here
+     *
+     * @param array $args
+     * @return User
+     */
+    public function givenASocialiteUser( $args = [] ): User
+    {
+        $abstractUser = Mockery::mock( 'Laravel\Socialite\Two\User' );
+
+        $default = [
+            'id' => 1,
+            'name' => '',
+            'email' => 'sample@sample.com',
+            'nickname' => '',
+            'avatar' => 'https://en.gravatar.com/userimage',
+        ];
+
+        $user = array_merge( $default, $args );
+
+        extract( $user );
+
+        // Get the api user object here
+        $abstractUser->shouldReceive( 'getId' )
+            ->andReturn( $id )
+            ->shouldReceive( 'getName' )
+            ->andReturn( $name )
+            ->shouldReceive( 'getEmail' )
+            ->andReturn( $email )
+            ->shouldReceive( 'getNickname' )
+            ->andReturn( $nickname )
+            ->shouldReceive( 'getAvatar' )
+            ->andReturn( $avatar )
+            ->shouldReceive( 'getRaw' )
+            ->andReturn( $user );
+
+        echo print_r( $abstractUser->user, true );
+
+        return $abstractUser;
+    }
 }
